896c3e27b33bdd355176f1a88fd8f3b2014dcd0e
likun <likun@gintong.com>
2013-04-17 17:01:58 +0800

    Signed-off-by: likun <likun@gintong.com>

--- phoenix-knowledge-provider/src/test/resources/sql/mysql/schema.sql
+++ phoenix-knowledge-provider/src/test/resources/sql/mysql/schema.sql
@@ -9,19 +9,20 @@ CREATE TABLE `tb_article` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `uid` bigint(20) DEFAULT NULL COMMENT 'phoenix_user.tb_user.id',
   `author` varchar(255) DEFAULT NULL COMMENT '文章作者，默认为当前登录用户的name',
-  `scource` varchar(255) DEFAULT NULL COMMENT '文章来源',
+  `source` varchar(255) DEFAULT NULL COMMENT '文章来源',
   `articleTitle` varchar(255) DEFAULT NULL COMMENT '文章标题',
-  `articlesContent` text COMMENT '文章内容',
+  `articleContent` text COMMENT '文章内容',
   `categoryid` bigint(20) DEFAULT NULL COMMENT 'phoenix_knowledge.tb_category.id',
   `sortId` varchar(255) DEFAULT NULL COMMENT 'phoenix_knowledge.tb_category.sortId',
-  `pubdate` varchar(0) DEFAULT NULL COMMENT '文章发布时间',
-  `modifyTime` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '最后修改时间',
+  `pubdate` timestamp NULL DEFAULT NULL COMMENT '文章发布时间',
+  `modifyTime` timestamp NULL DEFAULT NULL COMMENT '最后修改时间',
   `essence` varchar(255) DEFAULT '0' COMMENT '是否加精 0:否 1:是',
   `recycleBin` varchar(255) DEFAULT '0' COMMENT '是否标志为回收站文章0:否  1:是',
   `state` varchar(255) DEFAULT NULL,
   `clickNum` bigint(20) DEFAULT NULL COMMENT '文章点击次数',
+  `task_id` varchar(765) DEFAULT NULL COMMENT '与文章附件表关联',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 
 -- ----------------------------
@@ -33,27 +34,9 @@ CREATE TABLE `tb_category` (
   `uid` bigint(20) DEFAULT NULL COMMENT 'phoenix_user.tb_user.id',
   `categoryName` varchar(255) DEFAULT NULL COMMENT '分类名称',
   `parentId` bigint(20) DEFAULT '0' COMMENT '父类ID',
-  `sortId` varchar(255) DEFAULT NULL COMMENT '排序ID，三位一级 如001001,为一级分类下的第一个分类',
+  `sortId` varchar(255) DEFAULT NULL COMMENT '排序ID，九位一级 如000000001000000001,为一级分类下的第一个分类',
   `state` varchar(255) DEFAULT '0' COMMENT '分类状态 0:正常   1:删除',
-  `subtime` varchar(255) DEFAULT NULL COMMENT '分类创建时间',
-  `modtime` varchar(255) DEFAULT NULL COMMENT '最后修改时间',
+  `subtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `modtime` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- 文件meta信息表
--- ----------------------------
-DROP TABLE IF EXISTS `tb_file`;
-CREATE TABLE `tb_file` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `articleId` bigint(20) DEFAULT NULL COMMENT 'phoenix_knowledge.tb_article.id',
-  `showName` varchar(255) DEFAULT NULL COMMENT '文件名(文件原始名称，需在前端显示)',
-  `fiileName` varchar(255) DEFAULT NULL COMMENT '上传后生成的新的文件名称',
-  `extension` varchar(255) DEFAULT NULL COMMENT '文件扩展名',
-  `filePath` varchar(255) DEFAULT NULL COMMENT '附件路径',
-  `uptime` varchar(255) DEFAULT NULL COMMENT '上传时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
