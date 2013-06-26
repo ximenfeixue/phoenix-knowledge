2284ece6150b87bb08b2be81016bc8e4aadec706
likun <likun@gintong.com>
2013-06-26 14:26:06 +0800

    提高代码覆盖率
    Signed-off-by: likun <likun@gintong.com>

---
+++ phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/category/impl/CategoryHelper.java
@@ -0,0 +1,18 @@
+package com.ginkgocap.ywxt.knowledge.service.category.impl;
+
+public class CategoryHelper {
+	//给新增的分类生成新的sortId
+	public String generateSortId(String currentSortId) throws Exception{
+		try{
+			int clen = currentSortId.length();
+			String lastCurrentSort = currentSortId.substring(clen - 9 ,clen);
+			String temp = (Integer.parseInt(lastCurrentSort) + 1) + "";
+			String tempstr = "";
+			for(int i = 1; i <= (9-temp.length()); i ++)tempstr += "0";
+			String newSortId  = currentSortId.substring(0,clen - 9) + tempstr + temp;
+			return newSortId;
+		}catch(Exception e){
+			throw new Exception("generateSortId_err");
+		}
+	}
+}
