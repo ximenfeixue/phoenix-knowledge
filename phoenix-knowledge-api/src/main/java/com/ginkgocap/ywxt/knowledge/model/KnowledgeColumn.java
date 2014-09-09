a16a092684fca675980a4cb6e59dfa4d582f815e
shiguangyuan <shiguangyuan@gintong.com>
2014-09-09 10:03:51 +0800

    修改了knowledgeColumn
    Signed-off-by: shiguangyuan <shiguangyuan@gintong.com>

--- phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/model/KnowledgeColumn.java
+++ phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/model/KnowledgeColumn.java
@@ -3,96 +3,96 @@ package com.ginkgocap.ywxt.knowledge.model;
 import java.io.Serializable;
 import java.util.Date;
 
+
 /**
  *   
  * <p>知识栏目</p>  
  * <p>于2014-8-20 由 边智伟 创建 </p>
- * @author  <p>当前负责人 边智伟</p>     
+ * @author  <p>当前负责人 guangyuan</p>     
  * @since <p>1.2.1-SNAPSHOT</p> 
  *
  */
 public class KnowledgeColumn implements Serializable {
+    
     private static final long serialVersionUID = -5440480908931909931L;
-    private long id;//分类id
+    
+    private Long id;//分类id
     private String columnName;//分类名称
-    private int parentColumnId;//父分类id
-    private int createUserId;//用户id
+    private Long parentColumnId;//父分类id
+    private Long createUserId;//用户id
     private String tag;//分类标签，分号分割
-    private int level;//
+    private Integer level;//
     private String columnLevelPath;//分类层级路径
     private Date createTime;//创建时间
     private Date updateTime;//更新时间
-
-    public long getId() {
+    private Integer delStatus=0; //删除状态    0-正常  1-已删除
+    private Integer subscribeCount; //订阅数量
+//    private Object subscribers;  //订阅人列表
+    public Long getId() {
         return id;
     }
-
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
-
     public String getColumnName() {
         return columnName;
     }
-
     public void setColumnName(String columnName) {
         this.columnName = columnName;
     }
-
-    public int getParentColumnId() {
+    public Long getParentColumnId() {
         return parentColumnId;
     }
-
-    public void setParentColumnId(int parentColumnId) {
+    public void setParentColumnId(Long parentColumnId) {
         this.parentColumnId = parentColumnId;
     }
-
-    public int getCreateUserId() {
+    public Long getCreateUserId() {
         return createUserId;
     }
-
-    public void setCreateUserId(int createUserId) {
+    public void setCreateUserId(Long createUserId) {
         this.createUserId = createUserId;
     }
-
     public String getTag() {
         return tag;
     }
-
     public void setTag(String tag) {
         this.tag = tag;
     }
-
-    public int getLevel() {
+    public Integer getLevel() {
         return level;
     }
-
-    public void setLevel(int level) {
+    public void setLevel(Integer level) {
         this.level = level;
     }
-
     public String getColumnLevelPath() {
         return columnLevelPath;
     }
-
     public void setColumnLevelPath(String columnLevelPath) {
         this.columnLevelPath = columnLevelPath;
     }
-
     public Date getCreateTime() {
         return createTime;
     }
-
     public void setCreateTime(Date createTime) {
         this.createTime = createTime;
     }
-
     public Date getUpdateTime() {
         return updateTime;
     }
-
     public void setUpdateTime(Date updateTime) {
         this.updateTime = updateTime;
+    }
+    public Integer getDelStatus() {
+        return delStatus;
+    }
+    public void setDelStatus(Integer delStatus) {
+        this.delStatus = delStatus;
+    }
+    public Integer getSubscribeCount() {
+        return subscribeCount;
+    }
+    public void setSubscribeCount(Integer subscribeCount) {
+        this.subscribeCount = subscribeCount;
     }
 
 }
