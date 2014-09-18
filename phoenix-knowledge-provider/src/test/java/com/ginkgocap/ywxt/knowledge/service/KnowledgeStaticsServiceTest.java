20dba27b7883f3d84fd880e0c07453ba74595340
caihe <caihe@gintong.com>
2014-09-18 14:27:54 +0800

    修改bug Signed-off-by: caihe <caihe@gintong.com>

--- phoenix-knowledge-provider/src/test/java/com/ginkgocap/ywxt/knowledge/service/KnowledgeStaticsServiceTest.java
+++ phoenix-knowledge-provider/src/test/java/com/ginkgocap/ywxt/knowledge/service/KnowledgeStaticsServiceTest.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 
 import com.ginkgocap.ywxt.knowledge.base.TestBase;
-import com.ginkgocap.ywxt.knowledge.model.KnowledgeStatics;
+import com.ginkgocap.ywxt.knowledge.entity.KnowledgeStatics;
 import com.ginkgocap.ywxt.knowledge.service.knowledgecategory.KnowledgeCategoryService;
 import com.ginkgocap.ywxt.knowledge.service.knowledgestatics.KnowledgeStaticsService;
 
@@ -20,35 +19,35 @@ import com.ginkgocap.ywxt.knowledge.service.knowledgestatics.KnowledgeStaticsSer
 
 public class KnowledgeStaticsServiceTest extends TestBase {
 
-    public KnowledgeStaticsServiceTest() {
-        System.out.println(123456);
-    }
+	public KnowledgeStaticsServiceTest() {
+		System.out.println(123456);
+	}
 
-    @Autowired
-    private KnowledgeStaticsService knowledgeStaticsService;
+	@Autowired
+	private KnowledgeStaticsService knowledgeStaticsService;
 
-    @Autowired
-    private KnowledgeCategoryService knowledgeCategoryService;
+	@Autowired
+	private KnowledgeCategoryService knowledgeCategoryService;
 
-    @Test
-    public void testinsertKnowledgeR() {
+	@Test
+	public void testinsertKnowledgeR() {
 
-        KnowledgeStatics knowledgeStatics = new KnowledgeStatics();
-        knowledgeStatics.setKnowledgeid(53);
-        //		knowledgeStatics.setCollectionCount(0);
-        //		knowledgeStatics.setCommentCount(0);
-        //		knowledgeStatics.setShareCount(0);
-        //		knowledgeStatics.setClickCount(0);
-        knowledgeStaticsService.insertKnowledgeStatics(knowledgeStatics);
-    }
+		KnowledgeStatics knowledgeStatics = new KnowledgeStatics();
+		knowledgeStatics.setKnowledgeId((long) 111);
+		// knowledgeStatics.setCollectionCount(0);
+		// knowledgeStatics.setCommentCount(0);
+		// knowledgeStatics.setShareCount(0);
+		// knowledgeStatics.setClickCount(0);
+		knowledgeStaticsService.insertKnowledgeStatics(knowledgeStatics);
+	}
 
-    //测试获取评论排行列表
-    @Test
-    public void testSelectRankList() {
-        List<KnowledgeStatics> l = knowledgeStaticsService.selectRankList(1);
-        for (KnowledgeStatics k : l) {
-            System.out.println(k.getCommentCount());
-        }
-    }
+	// 测试获取评论排行列表
+	@Test
+	public void testSelectRankList() {
+		KnowledgeStatics knowledgeStatics = knowledgeStaticsService
+				.selectByknowledgeId(111);
+		System.out.println(knowledgeStatics.getKnowledgeId());
+
+	}
 
 }
