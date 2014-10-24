81ed57265efbd96a5c19968d4cd300ebb432014b
zhangzhen <zhangzhen@gintong.com>
2014-10-24 10:54:27 +0800

    移动组 加入mobileService接口
    Signed-off-by: zhangzhen <zhangzhen@gintong.com>

---
+++ phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/service/MobileSearchService.java
@@ -0,0 +1,25 @@
+package com.ginkgocap.ywxt.knowledge.service;
+
+import java.util.Map;
+
+public interface MobileSearchService {
+
+	/**
+	 * 关键词搜索
+	 * 
+	 * @param userid
+	 *            用户ID
+	 * @param keywords
+	 *            关键词
+	 * @param scope
+	 *            范围
+	 * @param pno
+	 *            当前页号
+	 * @param pszie
+	 *            每页请求条数
+	 * @return
+	 */
+	public Map<String, Object> searchByKeywords(Long userid, String keywords,
+			String scope, String pno, String pszie);
+
+}
