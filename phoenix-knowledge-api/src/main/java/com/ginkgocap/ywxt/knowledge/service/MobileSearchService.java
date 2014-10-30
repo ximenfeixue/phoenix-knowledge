4f39bd553f0ba5cc65624778ada14eb1b9f4a93d
zhangzhen <zhangzhen@gintong.com>
2014-10-30 13:20:27 +0800

    移动端搜索接口
    Signed-off-by: zhangzhen <zhangzhen@gintong.com>

--- phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/service/MobileSearchService.java
+++ phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/service/MobileSearchService.java
@@ -4,24 +4,15 @@ import java.util.Map;
 
 public interface MobileSearchService {
 
-	/**
-	 * 关键词搜索
-	 * 
-	 * @param userid
-	 *            用户ID
-	 * @param keywords
-	 *            关键词
-	 * @param scope
-	 *            范围
-	 * @param pno
-	 *            当前页号
-	 * @param pszie
-	 *            每页请求条数
-	 * @return
-	 */
+	/** 关键词搜索*/
 	public Map<String, Object> searchByKeywords(Long userid, String keywords,
 			String scope, String pno, String pszie);
-	
+
+	/** 根据标签与关键字搜索 */
 	public Map<String, Object> selectKnowledgeByTagsAndkeywords(Long userid,
 			String keywords, String scope, String tag, int page, int size);
+
+	/** 根据我的收藏与关键字搜索 */
+	public Map<String, Object> selectKnowledgeByMyCollectionAndkeywords(
+			Long userid, String keywords, String scope, int page, int size);
 }
