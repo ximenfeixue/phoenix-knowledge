92216818f2bee51fdb2e784d9eef97bc72b4920e
lihaiyan <lihaiyan@gintong.com>
2014-11-07 12:48:01 +0800

    Signed-off-by: lihaiyan <lihaiyan@gintong.com>

--- phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/AttachmentServiceImpl.java
+++ phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/AttachmentServiceImpl.java
@@ -1,10 +1,13 @@
 package com.ginkgocap.ywxt.knowledge.service.impl;
 
 import java.util.HashMap;
+
 import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.ginkgocap.ywxt.knowledge.entity.Attachment;
@@ -18,13 +21,20 @@ public class AttachmentServiceImpl implements AttachmentService {
 	@Resource
 	private AttachmentMapper attachmentMapper;
 
+	private final Logger logger = LoggerFactory
+			.getLogger(AttachmentServiceImpl.class);
+
 	@Override
 	public Map<String, Object> addAttachmentFile(Attachment att) {
+		logger.info("进入添加附件文件请求");
 		Map<String, Object> result = new HashMap<String, Object>();
 		int v = attachmentMapper.insertSelective(att);
+
 		if (v > 0) {
+			logger.info("添加附件到数据库请F求成功!");
 			result.put(Constants.status, Constants.ResultType.success.v());
 		} else {
+			logger.error("添加附件到数据库请求失败,用户：{}", att.getUserid());
 			result.put(Constants.status, Constants.ResultType.fail.v());
 		}
 		return result;
@@ -37,11 +47,12 @@ public class AttachmentServiceImpl implements AttachmentService {
 
 		Attachment att = new Attachment();
 		att.setFileName(fileName);
-		att.setFileSize(Long.parseLong(fileSize + ""));
+		att.setFileSize(Long.parseLong(fileSize == null ? "0" : fileSize + ""));
 		att.setDownloadUrl(downloadUrl);
 		att.setTaskid(taskId);
 		att.setUserid(userId);
 		att.setFileType(fileType);
+
 		return addAttachmentFile(att);
 	}
 
