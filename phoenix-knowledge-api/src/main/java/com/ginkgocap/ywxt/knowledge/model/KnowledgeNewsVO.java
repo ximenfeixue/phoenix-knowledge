5661fb90f969e71d28f775c16a73f9358526a8d7
caihe <caihe@gintong.com>
2014-11-11 21:09:27 +0800

    Signed-off-by: caihe <caihe@gintong.com>

--- phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/model/KnowledgeNewsVO.java
+++ phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/model/KnowledgeNewsVO.java
@@ -13,7 +13,7 @@ public class KnowledgeNewsVO implements Serializable {
 	private String taskId; // 知识附件
 	private String tags; // 知识
 	private String source; // 知识来源
-	private String catalogueIds; // 知识所在目录 (目录数组)
+	private String catalogueIds; // 知识所在目录 (目录数组) 格式(263,266,267,268)
 	private String columnid; // 知识所属栏目
 	private String essence; // 知识加精状态( 是否加精（0-不加 1-加）)
 	private String selectedIds; // 知识所属权限 格式(
