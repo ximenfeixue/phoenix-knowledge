3e9087d0eb7c454405ef2faa2a375c7187184b0e
zhangzhen <zhangzhen@gintong.com>
2014-11-14 19:41:24 +0800

    测试修改标签
    Signed-off-by: zhangzhen <zhangzhen@gintong.com>

--- phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/KnowledgeTagServiceImpl.java
+++ phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/KnowledgeTagServiceImpl.java
@@ -1,13 +1,36 @@
 package com.ginkgocap.ywxt.knowledge.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
-import com.ginkgocap.ywxt.knowledge.dao.knowledtag.KnowledgeTagDAO;
+import org.springframework.data.mongodb.core.query.Criteria;
+import com.ginkgocap.ywxt.knowledge.model.KnowledgeInvestment;
 import com.ginkgocap.ywxt.knowledge.service.KnowledgeTagService;
 
 @Service("knowledgeTagService")
 public class KnowledgeTagServiceImpl implements KnowledgeTagService {
+	
+	@Resource
+	private MongoTemplate mongoTemplate;
+
+	@Override
+	public boolean updateKnowledgeTag(long kid, int type, String tags) {
+		boolean result = false;
+		try {
+		Criteria c = Criteria.where("_id").is(kid);
+		Update update = new Update();
+		update.set("tags", tags);
+		Query query = new Query(c);
+		mongoTemplate.findAndModify(query, update, KnowledgeInvestment.class,"KnowledgeInvestment");
+		result = true;
+		} catch (Exception e) {
+		}
+		return result;
+	}
 
 //	@Autowired
 //	private KnowledgeTagDAO knowledgeTagDAO;
