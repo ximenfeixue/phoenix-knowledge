2a6041096204dcd021390467e0affcc1e7392e1a
caihe <caihe@gintong.com>
2014-11-25 16:21:27 +0800

    Signed-off-by: caihe <caihe@gintong.com>

--- phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/model/KnowledgeNews.java
+++ phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/model/KnowledgeNews.java
@@ -32,8 +32,7 @@ public class KnowledgeNews extends Knowledge {
 		this.setS_addr("");
 		this.setCpathid(vo.getColumnPath());
 		this.setPic(vo.getPic());
-		this.setDesc(StringUtils.substring(
-				HtmlToText.html2Text(vo.getContent()), 0, 50));
+		this.setDesc(HtmlToText.htmlTotest(vo.getContent()));
 		this.setContent(vo.getContent());
 		this.setEssence(Integer.parseInt(vo.getEssence()));
 		this.setCreatetime(vo.getCreatetime());
@@ -63,8 +62,7 @@ public class KnowledgeNews extends Knowledge {
 		this.setS_addr("");
 		this.setCpathid(vo.getColumnPath());
 		this.setPic(vo.getPic());
-		this.setDesc(StringUtils.substring(
-				HtmlToText.html2Text(vo.getContent()), 0, 50));
+		this.setDesc(HtmlToText.htmlTotest(vo.getContent()));
 		this.setContent(vo.getContent());
 		this.setEssence(Integer.parseInt(vo.getEssence()));
 		this.setCreatetime(vo.getCreatetime());
