f74cbb187cf5efb22ab4e894ff17bfb802b98935
zhangwei <zhangwei@gintong.com>
2014-11-26 17:09:25 +0800

    Signed-off-by: zhangwei <zhangwei@gintong.com>

--- phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/dao/news/impl/KnowledgeNewsDAOImpl.java
+++ phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/dao/news/impl/KnowledgeNewsDAOImpl.java
@@ -237,7 +237,8 @@ public class KnowledgeNewsDAOImpl implements KnowledgeNewsDAO {
 			update.set("selectedIds", vo.getSelectedIds());
 			update.set("asso", vo.getAsso());
 			update.set("tags", vo.getTags());
-
+			update.set("oid", vo.getOid());
+			update.set("synonyms", vo.getSynonyms());
 			mongoTemplate.updateFirst(query, update,
 					obj.substring(obj.lastIndexOf(".") + 1, obj.length()));
 
