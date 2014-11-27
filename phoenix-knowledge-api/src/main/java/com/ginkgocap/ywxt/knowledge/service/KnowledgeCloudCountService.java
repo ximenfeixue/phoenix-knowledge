b0968c958426eda706cd52d0a886b51802f4306f
zhangwei <zhangwei@gintong.com>
2014-11-27 19:54:16 +0800

    Signed-off-by: zhangwei <zhangwei@gintong.com>

---
+++ phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/service/KnowledgeCloudCountService.java
@@ -0,0 +1,14 @@
+package com.ginkgocap.ywxt.knowledge.service;
+
+
+/**
+ * 知识Service
+ * 
+ * @author zhangwei
+ * 
+ */
+public interface KnowledgeCloudCountService {
+	
+	public void countReader(Long kid);
+	
+}
---
+++ phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/KnowledgeCloudCountServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ginkgocap.ywxt.knowledge.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.ginkgocap.ywxt.knowledge.mapper.KnowledgeStaticsMapperManual;
+import com.ginkgocap.ywxt.knowledge.service.KnowledgeCloudCountService;
+import com.ginkgocap.ywxt.knowledge.util.Constants;
+
+@Service("KnowledgeCloudCountService")
+public class KnowledgeCloudCountServiceImpl implements KnowledgeCloudCountService {
+	
+	@Resource
+	private KnowledgeStaticsMapperManual knowledgeStaticsMapperManual;
+	
+	@Override
+	public void countReader(Long kid) {
+		// 添加点击数
+				knowledgeStaticsMapperManual.updateStatics(kid, 0, 0, 0,
+						Constants.StaticsValue.clickCount.v());
+	}
+
+
+}
