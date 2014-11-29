a4979a3f3b28058787f01188c0525f9af8e7e32a
zhangwei <zhangwei@gintong.com>
2014-11-29 14:30:13 +0800

    Signed-off-by: zhangwei <zhangwei@gintong.com>

--- phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/service/KnowledgeCloudCountService.java
+++ phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/service/KnowledgeCloudCountService.java
@@ -1,5 +1,10 @@
 package com.ginkgocap.ywxt.knowledge.service;
 
+import java.util.Map;
+
+import com.ginkgocap.ywxt.knowledge.model.Knowledge;
+import com.ginkgocap.ywxt.user.model.User;
+
 
 /**
  * 知识Service
@@ -10,5 +15,7 @@ package com.ginkgocap.ywxt.knowledge.service;
 public interface KnowledgeCloudCountService {
 	
 	public void countReader(Long kid);
+	
+	public Map<String,Object> isHavePremisson(Knowledge knowledge,User user);
 	
 }
