fc27b7ca633f72eaf4af6a32232861f9bb31432d
bianzhiwei <bianzhiwei@gintong.com>
2015-01-12 16:01:57 +0800

    新材料定制
    Signed-off-by: bianzhiwei <bianzhiwei@gintong.com>

--- phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/ColumnVisibleServiceImpl.java
+++ phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/ColumnVisibleServiceImpl.java
@@ -190,10 +190,48 @@ public class ColumnVisibleServiceImpl implements ColumnVisibleService {
         long cs = columnVisibleMapper.countByExample(cm);
         return cs;
     }
+    
+    //新栏目
+    @Override
+    public long countListByCidAndUserId(long userid, Long cid) {
+        ColumnVisibleExample cm = new ColumnVisibleExample();
+        com.ginkgocap.ywxt.knowledge.entity.ColumnVisibleExample.Criteria cri = cm.createCriteria();
+        cri.andUserIdEqualTo(userid);
+        if (cid != null) {
+            cri.andColumnIdEqualTo(cid);
+        }
+        long cs = columnVisibleMapper.countByExample(cm);
+        return cs;
+    }
 
     @Override
     public void init(long userid, long gtnid) {
         List<Column> l = columnValueMapper.selectByParams(null, Constants.gtnid, userid);
+        List<ColumnVisible> cvl = new ArrayList<ColumnVisible>();
+        for (Column c : l) {
+            if (!c.getColumnname().equals("未分组")) {
+                long id = c.getId();
+                long pcid = c.getParentId();
+                String cname = c.getColumnname();
+                ColumnVisible cv = new ColumnVisible();
+                cv.setColumnId(id);
+                cv.setPcid(pcid);
+                cv.setUserId(userid);
+                cv.setCtime(new Date());
+                cv.setUtime(new Date());
+                cv.setSortId(c.getColumnLevelPath());
+                cv.setColumnName(cname);
+                cv.setState((short) 0);
+                cvl.add(cv);
+            }
+        }
+        if (cvl.size() > 0) {// 批量插入
+            columnVisibleValueMapper.init(cvl);
+        }
+    }
+    @Override
+    public void initOne(long userid, long gid,Long cid) {
+        List<Column> l = columnValueMapper.selectByCid(cid,Constants.gtnid, userid);
         List<ColumnVisible> cvl = new ArrayList<ColumnVisible>();
         for (Column c : l) {
             if (!c.getColumnname().equals("未分组")) {
