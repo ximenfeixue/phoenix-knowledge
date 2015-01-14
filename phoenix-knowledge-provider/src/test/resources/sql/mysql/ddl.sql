194321cbed061d003ee091cd2c741e61c76bc632
bianzhiwei <bianzhiwei@gintong.com>
2015-01-14 13:49:45 +0800

    Signed-off-by: bianzhiwei <bianzhiwei@gintong.com>

--- phoenix-knowledge-provider/src/test/resources/sql/mysql/ddl.sql
+++ phoenix-knowledge-provider/src/test/resources/sql/mysql/ddl.sql
@@ -1,2 +1,7 @@
-DELETE  FROM tb_column_visible  WHERE  column_id IN(5,8,9,11)
+DELETE  FROM tb_column_visible  WHERE  column_id IN(5,8,9,11);
+UPDATE tb_column t
+SET t.columnName = '新材料',
+  t.path_name = '新材料',
+  t.del_status = 0
+WHERE t.id = 5
 	 
