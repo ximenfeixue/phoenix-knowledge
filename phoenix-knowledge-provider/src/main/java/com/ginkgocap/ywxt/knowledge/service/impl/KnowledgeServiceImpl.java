b55596796f9fbfdd874f951b2f57dda1dae8b8af
caihe <caihe@gintong.com>
2015-01-15 18:20:38 +0800

    Signed-off-by: caihe <caihe@gintong.com>

--- phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/KnowledgeServiceImpl.java
+++ phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/KnowledgeServiceImpl.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import net.sf.json.JSONObject;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -903,6 +905,9 @@ public class KnowledgeServiceImpl implements KnowledgeService {
 		// 删除知识之前的权限
 		userPermissionService.deleteUserPermission(vo.getkId(), user.getId());
 		// 添加知识到权限表.若是独乐（1），不入权限,直接插入到mongodb中
+		
+		JSONObject j = JSONObject.fromObject(vo.getSelectedIds().substring(1, vo.getSelectedIds().length()-1).replaceAll("\\\\",""));
+		vo.setSelectedIds(j.toString());
 		if (StringUtils.isNotBlank(vo.getSelectedIds())
 				&& !vo.getSelectedIds().equals(dule)) {
 			// 获取知识权限,大乐（2）：用户ID1，用户ID2...&中乐（3）：用户ID1，用户ID2...&小乐（4）：用户ID1，用户ID2...
