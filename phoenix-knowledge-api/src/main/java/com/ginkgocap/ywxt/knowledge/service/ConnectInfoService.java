fe495e807a1392ca77e0f216231bc8d071ed2fcf
bianzhiwei <bianzhiwei@gintong.com>
2015-01-27 11:05:29 +0800

    Signed-off-by: bianzhiwei <bianzhiwei@gintong.com>

--- phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/service/ConnectInfoService.java
+++ phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/service/ConnectInfoService.java
@@ -17,10 +17,12 @@ public interface ConnectInfoService {
 	 * @param kid 知识id
 	 * @param connType 关联类型 （为null时查询所有类型）
 	 * @param tag 默认“”标识全部
+	 * @param pType 关联人类型 默认为“”
+	 * @param pOff 关联人筛选好友偏移量
 	 * @param page 当前页
 	 * @param size 每页大小
 	 * @return
 	 */
-	public Map<String,Object> findConnectInfo(Long kid,Integer connType,String tag,int page, int size);
+	public Map<String,Object> findConnectInfo(Long kid,Integer connType,String tag, String pType, Integer pOff,int page, int size);
 
 }
