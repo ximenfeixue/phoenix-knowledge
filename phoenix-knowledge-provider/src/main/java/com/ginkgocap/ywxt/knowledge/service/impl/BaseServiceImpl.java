fab5bd1fd65868d1543cb044b81812eae12d5450
caihe <caihe@gintong.com>
2015-11-20 09:32:34 +0800

    大数据

--- phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/BaseServiceImpl.java
+++ phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/BaseServiceImpl.java
@@ -1,41 +1,28 @@
 package com.ginkgocap.ywxt.knowledge.service.impl;
 
-import net.sf.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import com.gintong.rocketmq.api.DefaultMessageService;
-import com.gintong.rocketmq.common.FlagTypeUtils;
-import com.gintong.rocketmq.common.RocketSendResult;
-import com.gintong.rocketmq.common.TopicType;
+import com.ginkgocap.ywxt.knowledge.thread.NoticeThreadPool;
+import com.ginkgocap.ywxt.knowledge.util.Constants;
 
-@Service
-public class BaseServiceImpl {
+public class BaseServiceImpl{
 
-	@Autowired(required = true)
-	private DefaultMessageService defaultMessageService ;
-
+	@Autowired(required =true)
+	private NoticeThreadPool noticeThreadPool;
 	/**
 	 * 大数据通知接口
 	 * 
+	 * @param vo
+	 * @param noticeType
 	 */
-	public void noticeDataCenter(String noticeType, Object bean) {
-
-		String flagType = "";
-		if (StringUtils.equals("upd", noticeType)) {
-			flagType = FlagTypeUtils.updateKnowledgeFlag();
-		} else if (StringUtils.equals("add", noticeType)) {
-			flagType = FlagTypeUtils.createKnowledgeFlag();
-		} else if (StringUtils.equals("del", noticeType)) {
-			flagType = FlagTypeUtils.deleteKnowledgeFlag();
-		}
-		defaultMessageService.sendMessage(TopicType.KNOWLEDGE_TOPIC, flagType, beanToJson(bean));
-	}
-
-	public static String beanToJson(Object bean) {
-		JSONObject json = JSONObject.fromObject(bean);
-		return json.toString();
+	public void noticeDataCenter(String knowledgeType,Long knowledgeId, String noticeType) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("oper", noticeType);
+		params.put("type", knowledgeType);
+		params.put("kId", knowledgeId);
+		noticeThreadPool.noticeDataCenter(Constants.noticeType.knowledge.v(), params);
 	}
 }
