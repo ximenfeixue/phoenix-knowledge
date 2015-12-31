282b11981e4a0d8857dbf2d880fef1e07189954e
qianmingjin <qianmingjin@gintong.com>
2015-12-31 16:36:36 +0800

    新知识

--- phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/mapper/KnowledgeDraftValueMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ginkgocap.ywxt.knowledge.mapper;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface KnowledgeDraftValueMapper {
-
-	int deleteKnowledge(@Param("knowledgeids") long[] knowledgeids,
-			@Param("userid") long userid);
-}
