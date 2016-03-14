b1a7d2207add6b828217301016a3c075305a0963
liuxiaobo <liuxiaobo@jintong.com>
2016-03-14 16:50:30 +0800

    修改httpmethod

--- phoenix-knowledge-ui/src/main/java/com/ginkgocap/ywxt/knowledge/controller/knowledge/ColumnCustomController.java
+++ phoenix-knowledge-ui/src/main/java/com/ginkgocap/ywxt/knowledge/controller/knowledge/ColumnCustomController.java
@@ -10,7 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 import net.sf.json.JSONObject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ginkgocap.ywxt.knowledge.controller.BaseController;
@@ -27,16 +29,11 @@ public class ColumnCustomController extends BaseController {
 	@Resource
 	private IColumnCustomService columnCustomService;
 	
-	@RequestMapping("showColumn")
+	@RequestMapping(value="/showColumn/{pid}",method = RequestMethod.GET)
 	@ResponseBody
-	public InterfaceResult<List<ColumnCustom>> showColumn(HttpServletRequest request, HttpServletResponse response) throws Exception{
+	public InterfaceResult<List<ColumnCustom>> showColumn(HttpServletRequest request, HttpServletResponse response,@PathVariable Long pid) throws Exception{
 		InterfaceResult<List<ColumnCustom>> result=InterfaceResult.getInterfaceResultInstance(CommonResultCode.SUCCESS);
 		User user = this.getUser(request);
-		JSONObject requestJson = this.getRequestJson(request);
-		if(!requestJson.containsKey("pid")){
-			return InterfaceResult.getInterfaceResultInstance(CommonResultCode.PARAMS_EXCEPTION);
-		}
-		Long pid=requestJson.getLong("pid");
 		List<ColumnCustom> list=null;
 		long uid=0l;
 		if(user!=null){
@@ -48,7 +45,7 @@ public class ColumnCustomController extends BaseController {
 		return result;
 	}
 	
-	@RequestMapping("addColumn")
+	@RequestMapping(value="addColumn",method = RequestMethod.POST)
 	@ResponseBody
 	public InterfaceResult<ColumnCustom> addColumn(HttpServletRequest request, HttpServletResponse response) throws Exception{
 		InterfaceResult<ColumnCustom> result=InterfaceResult.getInterfaceResultInstance(CommonResultCode.SUCCESS);
@@ -77,7 +74,7 @@ public class ColumnCustomController extends BaseController {
 		return result;
 	}
 	
-	@RequestMapping("updateColumn")
+	@RequestMapping(value="",method = RequestMethod.PUT)
 	@ResponseBody
 	public InterfaceResult<ColumnCustom> updateColumn(HttpServletRequest request, HttpServletResponse response) throws Exception{
 		InterfaceResult<ColumnCustom> result=InterfaceResult.getInterfaceResultInstance(CommonResultCode.SUCCESS);
@@ -94,18 +91,16 @@ public class ColumnCustomController extends BaseController {
 		return result;
 	}
 	
-	@RequestMapping("deleteColumn")
+	@RequestMapping(value="/{id}",method = RequestMethod.DELETE)
 	@ResponseBody
-	public InterfaceResult<ColumnCustom> deleteColumn(HttpServletRequest request, HttpServletResponse response) throws Exception{
+	public InterfaceResult<ColumnCustom> deleteColumn(HttpServletRequest request, HttpServletResponse response,@PathVariable Long id) throws Exception{
 		InterfaceResult<ColumnCustom> result=InterfaceResult.getInterfaceResultInstance(CommonResultCode.SUCCESS);
 		User user = this.getUser(request);
 		JSONObject requestJson = this.getRequestJson(request);
-		//{"columnid":"2854"}:
-		if(!requestJson.containsKey("columnid")){
+		if(id==null){
 			return InterfaceResult.getInterfaceResultInstance(CommonResultCode.PARAMS_EXCEPTION);
 		}
-		Long columnid=requestJson.getLong("columnid");
-		this.columnCustomService.del(columnid);
+		this.columnCustomService.del(id);
 		return result;
 	}
 
