debc300ba284463d0ceea73c15e135101b98063f
Chen peifeng <chenpeifeng@gintong.com>
2016-07-25 14:01:46 +0800

    change method

--- phoenix-knowledge-ui/src/main/java/com/ginkgocap/ywxt/knowledge/v1/KnowledgeControllerV1.java
+++ phoenix-knowledge-ui/src/main/java/com/ginkgocap/ywxt/knowledge/v1/KnowledgeControllerV1.java
@@ -38,6 +38,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -48,8 +49,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.*;
 
-//@Controller
-//@RequestMapping("/knowledgeV1")
+@Controller
+@RequestMapping("/knowledgeV1")
 public class KnowledgeControllerV1 extends BaseController {
 
 	private final Logger logger = LoggerFactory.getLogger(KnowledgeControllerV1.class);
