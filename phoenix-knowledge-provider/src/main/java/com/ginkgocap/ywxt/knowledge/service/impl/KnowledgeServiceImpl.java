5ca9d2bc3d3d831c95477e1a58a1db7cc8fa8e1f
chenpeifeng <chenpeifeng@gintong.com>
2016-09-28 09:39:23 +0800

    chang code

--- phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/KnowledgeServiceImpl.java
+++ phoenix-knowledge-provider/src/main/java/com/ginkgocap/ywxt/knowledge/service/impl/KnowledgeServiceImpl.java
@@ -308,7 +308,7 @@ public class KnowledgeServiceImpl implements KnowledgeService, KnowledgeBaseServ
         //知识详细表删除
         try {
             int reslut = this.knowledgeMongoDao.deleteByIdAndColumnId(knowledgeId, columnType);
-            if (reslut <= 0) {
+            if (reslut < 0) {
                 logger.error("知识详细表删除失败: knowledgeId: " + knowledgeId + " columnType: " + columnType);
                 return InterfaceResult.getInterfaceResultInstance(CommonResultCode.PARAMS_DB_OPERATION_EXCEPTION, "知识详细删除失败!");
             }
