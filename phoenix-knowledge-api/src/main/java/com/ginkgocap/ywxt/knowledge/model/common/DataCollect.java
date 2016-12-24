21be25f9ebdb09163402eb7fae5a73d91f6400f5
Chen peifeng <chenpeifeng@gintong.com>
2016-12-24 18:48:04 +0800

    fix picture url

--- phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/model/common/DataCollect.java
+++ phoenix-knowledge-api/src/main/java/com/ginkgocap/ywxt/knowledge/model/common/DataCollect.java
@@ -377,8 +377,8 @@ public class DataCollect implements Serializable
 
         for (String url : imgUrl) {
             if (StringUtils.isNotBlank(url) && !"null".equals(url)) {
-                if (url.indexOf(".jpg") > 0 || url.indexOf(".jpeg") > 0 || url.indexOf(".gif") > 0 ||
-                        url.indexOf(".JPG") > 0 || url.indexOf(".JPEG") > 0 || url.indexOf(".GIF") > 0) {
+                if (url.indexOf(".jpg") > 0 || url.indexOf(".jpeg") > 0 || url.indexOf(".gif") > 0 || url.indexOf(".png") > 0 ||
+                        url.indexOf(".JPG") > 0 || url.indexOf(".JPEG") > 0 || url.indexOf(".GIF") > 0 || url.indexOf(".PNG") > 0 ) {
                     return url;
                 }
             }
