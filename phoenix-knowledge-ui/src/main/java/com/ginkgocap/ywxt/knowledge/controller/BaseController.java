3bad542df2e18dee304fd663e649d499b359c2be
Chen peifeng <chenpeifeng@gintong.com>
2017-04-14 20:35:46 +0800

    fix bug

--- phoenix-knowledge-ui/src/main/java/com/ginkgocap/ywxt/knowledge/controller/BaseController.java
+++ phoenix-knowledge-ui/src/main/java/com/ginkgocap/ywxt/knowledge/controller/BaseController.java
@@ -93,7 +93,7 @@ public abstract class BaseController {
 
     protected boolean isAdmin(HttpServletRequest request) {
         User user = this.getUser(request);
-        return (user != null && user.getId() == 0);
+        return (user != null && user.getId() == 1);
     }
 
     protected String getJsonIn(HttpServletRequest request) throws IOException {
