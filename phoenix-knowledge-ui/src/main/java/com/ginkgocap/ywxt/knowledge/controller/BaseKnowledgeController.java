9393f6395667917cbb84a3b6ebf953bdc4a6186c
Chen peifeng <chenpeifeng@gintong.com>
2017-11-29 15:18:35 +0800

    解决问题：知识内容比较短时创建失败。原因：知识内容经过html过滤后，内容变短。

--- phoenix-knowledge-ui/src/main/java/com/ginkgocap/ywxt/knowledge/controller/BaseKnowledgeController.java
+++ phoenix-knowledge-ui/src/main/java/com/ginkgocap/ywxt/knowledge/controller/BaseKnowledgeController.java
@@ -430,7 +430,7 @@ public abstract class BaseKnowledgeController extends BaseController {
         resourceMessage.setType(KnowledgeUtil.parserShortType(detail.getColumnType()));
         resourceMessage.setTitle(detail.getTitle());
         final String content = HtmlToText.htmlToText(detail.getContent());
-        final int totalLength = detail.getContent().length();
+        final int totalLength = content.length();
         final int length = totalLength >= 250 ? 250 : totalLength;
         final String contentDes = content.substring(0, length-1);
         resourceMessage.setDesc(contentDes);
